ee9a0893d904db4869bde732c49b2142694f759b
Yash Gajjar <yashgajjar26082001@gmail.com>
2025-10-24 11:56:16 +0530

    added new code file

--- Array/Medium/subarray_print_max_sum.py
+++ Array/Medium/subarray_print_max_sum.py
@@ -1,1 +1,14 @@
 nums =  [2, 3, 5, -2, 7, -4]
+
+sum = nums[0]
+new = []    
+
+for i in range(len(nums)):
+    current_sum = 0
+    for j in range(i, len(nums)):
+        current_sum += nums[j]
+        sum = max(sum, current_sum)
+        if sum == current_sum:
+            new = nums[i:j+1]
+
+print(f"Subarray with maximum sum {sum}: {new}")
