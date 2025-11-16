32e7e324be0bcb8424321b3b6466421870b34933
Yash Gajjar <yashgajjar26082001@gmail.com>
2025-11-16 23:05:26 +0530

    added 2 problems under hard

---
+++ SW2P/Hard/min_window_substring.py
@@ -0,0 +1,22 @@
+s = "ADOBECODEBANC" 
+t = "ABC"
+
+
+
+
+
+
+
+
+# unique_ele = len(s)
+
+
+# for i in range(len(s)):
+#     for j in range(i + 1, len(s) + 1):
+#         substring = s[i:j]
+#         if t in substring:
+#             length = len(substring)
+    
+#     min_length = min(length, len(s))
+    
+# print(min_length)
--- SW2P/Hard/subarray_k_diff_integer.py
+++ SW2P/Hard/subarray_k_diff_integer.py
@@ -1,4 +1,17 @@
-nums = [1, 2, 1, 2, 3]
-k = 2
+# nums = [1, 2, 1, 2, 3]
+# k = 2
 
+nums = [1, 2, 1, 3, 4]
+k = 3  
+n = len(nums)
+subarray = []
+count = 0
+
+for i in range(n):
+    for j in range(i, n):
+        current_subarray = nums[i : j + 1]
+        if len(set(current_subarray)) == k:
+            count += 1
+            
+print(count)
   
